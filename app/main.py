de427bb5e9e0af0bdc38c617e533889239bdfa8e
ayakatakuriko <warangkana0601@yahoo.ne.jp>
2023-01-23 18:40:11 +0900

    Remove last \n from echoed string

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def main():
         output = sys.stderr
     completed_process = subprocess.run([command, *args], capture_output=True)
     
-    print(completed_process.stdout.decode("utf-8"), file=output)
+    print(completed_process.stdout.decode("utf-8").strip(), file=output)
     
 
 
