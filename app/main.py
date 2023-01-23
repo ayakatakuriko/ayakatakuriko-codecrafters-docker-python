ab7d7a0ef6b5213a2f725032645cdf1cf4b82c32
ayakatakuriko <warangkana0601@yahoo.ne.jp>
2023-01-23 18:50:31 +0900

    Handle exit codes

--- app/main.py
+++ app/main.py
@@ -5,15 +5,19 @@ import sys
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
-    output = sys.stdout
+    output_f = sys.stdout
+    exit_s = 0
     
     if args[0] == "echo_stderr":
         args[0] = "echo"
-        output = sys.stderr
+        output_f = sys.stderr
+    elif args[0] == "exit":
+        exit_s = int(args[1])
+        
     completed_process = subprocess.run([command, *args], capture_output=True)
     
-    print(completed_process.stdout.decode("utf-8").strip(), file=output)
-    
+    print(completed_process.stdout.decode("utf-8").strip(), file=output_f)
+    exit(exit_s) 
 
 
 if __name__ == "__main__":
